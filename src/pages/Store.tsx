2989a186bfec0def0c1bb5c2fed57501df4b1a75
Pavel <sosnovskiy.pavel@inbox.ru>
2022-07-23 23:40:59 +0300

    Update Store.tsx

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,22 @@
+import { Col, Row } from "react-bootstrap"
+import storeItems from "../data/items.json"
 export const Store = () => {
     return (
-        <h1>
-            Store-1
-        </h1>
+        <>
+            <h1>
+                Store-1
+            </h1>
+            <Row>
+                {
+                    storeItems.map(item=>{
+                        return(
+                            <Col>
+                                {JSON.stringify(item)}
+                            </Col>
+                        )
+                    })
+                }
+            </Row>
+        </>
     )
 }
