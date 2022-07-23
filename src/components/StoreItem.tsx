fb16d6d4f0f03f6c5f876c3bf8397d52f4e079ec
Pavel <sosnovskiy.pavel@inbox.ru>
2022-07-23 23:57:29 +0300

    Update StoreItem.tsx

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,6 +1,16 @@
-export const StoreItem = () => {
+import { Card } from "react-bootstrap"
+
+type StoreItemProps = {
+    id: number
+    name: string
+    price: number
+    imgURL: string
+}
+
+export const StoreItem: React.FC<StoreItemProps> = ({id, name, price, imgURL}) => {
     return (
-        <>
-        </>
+        <Card>
+            <Card.Img variant="top" src={imgURL} height="220px" style={{objectFit: "cover"}}/>
+        </Card>
     )
 }
