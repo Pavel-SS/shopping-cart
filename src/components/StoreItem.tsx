0fc24bfff23dd546736047b9756097aab6bc83cd
Pavel <sosnovskiy.pavel@inbox.ru>
2022-07-24 00:23:45 +0300

    Update StoreItem.tsx

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap"
+import { currencyFormat } from "../util/currencyFormat"
 
 type StoreItemProps = {
     id: number
@@ -12,7 +13,14 @@ export const StoreItem: React.FC<StoreItemProps> = ({id, name, price, imgURL}) =
         <Card>
             <Card.Img variant="top" src={imgURL} height="220px" style={{objectFit: "cover"}}/>
             <Card.Body className="d-flex flex-column">
-                <Card.Title className="d-flex justify-content-space-between"></Card.Title>
+                <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+                    <span className="fs-3">
+                        {name}
+                    </span>
+                    <span className="text-black-50">
+                        {currencyFormat(price)}
+                    </span>
+                </Card.Title>
             </Card.Body>
         </Card>
     )
