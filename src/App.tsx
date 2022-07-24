e70df3d388de7b4ace9d196c9fc877cd9298e2ac
Pavel <sosnovskiy.pavel@inbox.ru>
2022-07-24 14:10:43 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Container } from "react-bootstrap"
 import { Route, Routes } from "react-router-dom"
 import { Navbar } from "./components/Navbar"
+import { ShoppingCardProvider } from "./context/shoppingCardContext"
 import { About } from "./pages/About"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 
 function App() {
- return(
-   <>
+ return (
+   <ShoppingCardProvider>
      <Navbar />
      <Container className="mb-4">
        <Routes>
@@ -16,7 +17,7 @@ function App() {
          <Route path="/about" element={<About />}/>
        </Routes>
      </Container>
-   </>
+   </ShoppingCardProvider>
  )
 }
 
